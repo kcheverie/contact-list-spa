7e8cbb43778a329f18c4b65d2b41941ea459ebae
kristincheverie <kcheverie@gmail.com>
2016-05-03 22:44:27 +0000

    add functionality

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,30 @@
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  content_type :json
+  @contacts = Contact.all
+  @contacts.to_json
+end
+
+post '/contacts' do
+  content_type :json
+  @contact = Contact.new(
+    name: params[:contact][:name],
+    email: params[:contact][:email],
+    phone: params[:contact][:phone]
+    )
+
+  if @contact.save
+    @contact.to_json
+  else
+    @contact.errors.to_json
+  end
+end
+
+get '/search' do
+  content_type :json
+  @contacts = Contact.where("name like ? OR email LIKE ?", "%#{params[:search_term]}%", "%#{params[:search_term]}%")
+  @contacts.to_json
+end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,60 @@
 $(document).ready(function() {
+  var $table = $('#contact-table');
+  var data = [];
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+  function renderTable() {
+    var $tableBody = $table.find('tbody');
+    $tableBody.html('');
+
+    data.forEach(function(contact) {
+      var html = "<tr>" +
+              "<td>" + contact.id + "</td>" +
+              "<td>" + contact.name + "</td>" +
+              "<td>" + "<a href='mailto:" + contact.email + "'>" + contact.email + "</a>" + "</td>" +
+              "<td>" + contact.phone + "</td>" +
+              "</tr>";
+      $('tbody').append(html); 
+    });
+  }
+
+  $.ajax({
+    url: '/contacts',
+    method: 'get',
+    headers: { accept: 'application/json' }
+  }).done(function(res) {
+    data = res;
+    renderTable();
+  });
+
+  $('#submit').on('click', function(e) {
+    e.preventDefault();
+    var newContact = {
+      name: $('#name').val(),
+      email: $('#email').val(),
+      phone: $('#phone').val()
+    };
+    $.ajax({
+      url: '/contacts',
+      method: 'POST',
+      headers: { accept: 'application/json' },
+      data: { contact: newContact}
+    }).done(function(res) {
+      data.push(res);
+      renderTable();
+    });
+  });
+
+  $('#submit-search').on('click', function(e) {
+    e.preventDefault();
+    var search = $('#search_term').val()
+    $.ajax({
+      url: '/search',
+      method: 'get',
+      headers: { accept: 'application/json' },
+      data: { "search_term": search }
+    }).done(function(res) {
+      data = res;
+      renderTable();
+    });
+  });
+})
